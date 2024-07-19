3b6b65016435f5d70e8d3f24178bea5e20d1b92d
snowman12320 <snowman12320@gmail.com>
2024-07-19 11:00:15 +0800

    fix: types mkdir

--- src/components/CountBtn.tsx
+++ src/components/CountBtn.tsx
@@ -10,7 +10,7 @@ export default function CountBtn({ className }: Props) {
   const [count, setCount] = useState(0);
 
   return (
-    <Button onClick={() => setCount((count) => count + 1)} className={cn('', className)}>
+    <Button size={'lg'} onClick={() => setCount((count) => count + 1)} className={cn('', className)}>
       Count is: {count}
     </Button>
   );
