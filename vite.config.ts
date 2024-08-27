5c2dc80579deabd16eafb27743e983611e6fbcda
snowman12320 <snowman12320@gmail.com>
2024-08-27 10:15:30 +0800

    chore: Update deploy.sh for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // vercel
-  // https://react-yahoo-message-nojs2azmf-snowman12320s-projects.vercel.app/
-  // gh-page
-  // base: process.env.NODE_ENV === 'production' ? '/react-yahoo-message/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
